66f1a0c953ffc9b2ed2300dcd269b3b82685a39b
Minghui <m.h.wang@live.com>
2018-04-26 15:40:20 -0400

    ensemble rank standardize option

--- R/ensemble.rank.R
+++ R/ensemble.rank.R
@@ -1,21 +1,20 @@
 #To compute composite rank score from multiple variables
 #Author: Minghui Wang <m.h.wang@live.com>
 #
-ensemble_rank=function(x,method=c('ProductOfRank','MeanOfLog','MeanOfLogLog'),small=1.0e-320){
+ensemble_rank=function(x,method=c('ProductOfRank','MeanOfLog','MeanOfLogLog'),small=1.0e-320,standardize=TRUE){
 	method=match.arg(method)
 	x=as.matrix(x)
 	if(method=='ProductOfRank'){
 		G=apply(x,2,function(y) {z=rank(y,ties.method = 'max');M=max(z);(M+1-z)/M} )
 		S=apply(G,1, prod)
-		S/max(S)
 	}else if(method=='MeanOfLog'){
 		G=-log(x+small)
 		S=rowSums(G,na.rm=TRUE)/ncol(G)
-		S/max(S)
 	}else if(method=='MeanOfLogLog'){
 		G=log(-log(x+small)+small)
 		G[G<0]=0
 		S=rowSums(G,na.rm=TRUE)/ncol(G)
-		S/max(S)
 	}
+	if(standardize) S=S/max(S)
+	S
 }
